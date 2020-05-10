404f7ce8b2d32222447b5d2f1500d63495bb044a
nguyenthaituan <12c6nguyenthaituan@gmail.com>
2020-05-11 01:33:12 +0700

    BaiTapJava_Creational

---
+++ src/BaiTap2/BaiTap2Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaiTap2;
+
+/**
+ *
+ * @author Admin
+ */
+public class BaiTap2Main {
+
+    public static void main(String[] args){   
+        MyStringBuilder str = new MyStringBuilder.Builder()
+                                .addString("Khai báo số:  ")
+                                .addFloat((float)9.9)
+                                .addString(" ; Thêm biểu thức: ")
+                                .addBool(Boolean.TRUE)
+                                .Builder();
+        
+        System.out.println( str.toString());
+    }
+    
+}
